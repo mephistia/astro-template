a3214c9966e1545308f67c8dac7fca8c6346c92c
Julia Stefanello <julia.stefanello@cwi.com.br>
2024-01-24 17:08:44 -0300

    feat: :wrench: update astro config file to add build assets prefix option

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
 
 import react from '@astrojs/react'
+
+const TEMP_LOCAL_URL = 'http://localhost:4321/'
+
+const { PUBLIC_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
+const site = import.meta.env.DEV ? TEMP_LOCAL_URL : PUBLIC_URL
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site,
+  build: {
+    assetsPrefix: PUBLIC_URL,
+  },
 })
